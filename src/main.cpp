92a1653a71c516691016956a71e561c270ab3727
devEROp <funanetoshiki@yahoo.co.jp>
2023-05-04 23:48:32 +0900

    20230504Hello

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+void Sex(int a,int b,int c,int d){
+  analogWrite(5,a);
+  analogWrite(6,b);
+  analogWrite(10,c);
+  analogWrite(11,d);
+}
+
+void setup() {
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+}
+
+void loop() {
+  Sex(3,3,3,3);
+}
