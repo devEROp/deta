4d8fb3ee744d81cb76634d8db6a3385c56fc00cd
devEROp <funanetoshiki@yahoo.co.jp>
2023-05-06 15:21:25 +0900

    fuck

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
-void Sex(int a,int b,int c,int d){
+int vddr;
+int vddl;
+int Sex(int a,int b,int c,int d){
   analogWrite(5,a);
   analogWrite(6,b);
   analogWrite(10,c);
@@ -11,9 +13,21 @@ void setup() {
   pinMode(6,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
-  
 }
-
 void loop() {
-  Sex(3,3,3,3);
+  vddr = int(analogRead(A0));
+  vddl = int(analogRead(A1));
+  Serial.print("vddr== "); 
+  Serial.print(vddr);
+  Serial.println();
+  Serial.print("vddl== "); 
+  Serial.println(vddl);
+  if(vddr > 323 && vddl <= 323){
+      Sex(222,0,111,0);
+  }else if(vddr <= 323 && vddl > 323){
+      Sex(111,0,222,0);
+  }else{
+    Sex(111,0,111,0);
+  }
+  delay(10);
 }
